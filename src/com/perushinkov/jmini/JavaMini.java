8610f7e6dd4d2bd715e2f18bee053936525632d0
perushinkov <fantasmo3@gmail.com>
2015-01-16 12:22:25 +0200

    Now project is run from console.
    java -jar minijava <file_name_1> <file_name_2> ...

--- src/com/perushinkov/jmini/JavaMini.java
+++ src/com/perushinkov/jmini/JavaMini.java
@@ -41,19 +41,39 @@ public class JavaMini {
     }
 
     public static void main(String[] args) {
-        String[] inputFiles = new String[] {"Factorial", "BinarySearch", "BinaryTree",
-                "BubbleSort", "LinearSearch", "LinkedList", "QuickSort", "TreeVisitor"};
+        //String[] inputFiles = new String[] {"Factorial", "BinarySearch", "BinaryTree",
+        //        "BubbleSort", "LinearSearch", "LinkedList", "QuickSort", "TreeVisitor"};
 
-        for (String filename: inputFiles) {
-            JavaMini example = new JavaMini(new File("resources/" + filename + ".java"));
+        for (String filename: args) {
+            System.out.println("File: " + args[0] + "\n");
+            //JavaMini example = new JavaMini(new File("resources/" + filename + ".java"));
+            JavaMini example = new JavaMini(new File(filename));
             TreeMap<ErrorCode, TokenPosition> tm = example.getLexer().getErrorMessages();
+            System.out.println("\tLex errors:");
             for (ErrorCode err: tm.keySet()) {
                 System.out.println(err.toString() + ":" + tm.get(err).toString());
             }
+            System.out.println();
+            System.out.println("\tParse errors:");
             List<String> errorList = example.getParser().getErrors();
             for (String error: errorList) {
                 System.out.println(error);
             }
+            System.out.println();
+            System.out.println("\tList of all tokens:");
+            List<Token> tokens = example.getLexer().getTokens();
+            int k = 0;
+            for (Token token : tokens) {
+                k++;
+                if (k == 5) {
+                    k = 0;
+                    System.out.println();
+                }
+                System.out.print("\t<" + token.getType().name() + "> ");
+                //System.out.println(token.toString());
+            }
+
+            System.out.println("\n\n\n");
         }
     }
 }
